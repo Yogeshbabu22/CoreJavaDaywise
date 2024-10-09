12bf0b3ec46efe49eeba56f6cb8058ca3fd0981a
HP <yogeshbehera120@gmail.com>
2024-10-09 12:37:41 +0530

    My First Comment

--- src/main/java/com/JavaPrograms/p1/TestProgram.java
+++ src/main/java/com/JavaPrograms/p1/TestProgram.java
@@ -1,5 +1,11 @@
 package com.JavaPrograms.p1;
 
-public class TestProgram {
+public class TestProgram  {
+	// TODO Auto-generated constructor stub
+public static void main(String[] args) {
+	System.out.println("Hi from Eclipse....");
+	
+	
+}
 
 }
